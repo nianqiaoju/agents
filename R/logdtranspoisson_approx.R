9ac41f5a397a44e6887a5e722167c1fc6eb46142
Phyllis Ju <12590785+nianqiaoju@users.noreply.github.com>
2021-05-12 14:12:43 -0400

    update document for translated Poisson approximation

--- R/logdtranspoisson_approx.R
+++ R/logdtranspoisson_approx.R
@@ -1,16 +1,17 @@
 #' @title translated Poisson approximation 
-#' @description density approximation of the Poission binomial distribution in log scale
+#' @description density approximation of the Poission binomial distribution in log scale. \\
+#' NOTE: the densities do not sum to 1.
 #' @param alpha vector of Bernoulli parameters
 #' @param xx  default is NULL, when x == NULL, output is the entire density function 
-#' @return log-density 
+#' @return approximate log densities. 
 #'
 #' @export
 
 logdtranspoisson_approx <- function(alpha, y = NULL){
-  mu <- sum(alpha)
-  sigma2 <- mu - sum(alpha**2)
-  translation <- floor(mu - sigma2)
-  rate <- mu - translation
-  if (is.null(y)) y <- c(0 : length(alpha))
-  return(dpois(x =  y - translation, lambda = rate, log = TRUE))
+  mu <- sum(alpha);
+  sigma2 <- mu - sum(alpha**2);
+  translation <- floor(mu - sigma2);
+  rate <- mu - translation;
+  if (is.null(y)) y <- c(0 : length(alpha));
+  return(dpois(x =  y - translation, lambda = rate, log = TRUE));
 }
