6f13787c892a980f8d6bcffb906e3c7088284e57
Phyllis Ju <12590785+nianqiaoju@users.noreply.github.com>
2021-05-17 13:11:10 -0400

    chg: fix bugs in sir_get_alpha.R

--- R/sir_get_alpha.R
+++ R/sir_get_alpha.R
@@ -9,8 +9,8 @@
 sir_get_alpha_sir <- function(agent_states, model_config){
 	alpha_matrix <- matrix(NA, nrow = 3, ncol = model_config$N);
 	## probability of recovery
-	alpha_matrix[3,] <- (agent_states == 2) + (agent_states ==1 ) * model_config$gamma
-	alpha_matrix[2,] <- sir_get_alpha_i(agent_state, model_config);
+	alpha_matrix[3,] <- (agent_states == 2) + (agent_states == 1) * model_config$gamma
+	alpha_matrix[2,] <- sir_get_alpha_i(agent_states, model_config);
 	alpha_matrix[1,] <- 1 - alpha_matrix[3,] - alpha_matrix[2,];
 	return(alpha_matrix)
 }
@@ -33,6 +33,11 @@ sir_get_alpha_i <- function(agent_states, model_config){
 #' @return \code{sir_get_alpha_plusone(agent_states, model_config)} returns a vector, whose entries are the probabilities that p(x[t+1,k] = x[t,k] + 1);
 #' @export
 sir_get_alpha_plusone <- function(agent_states, model_config){
-  alpha_plusone <- (agent_states == 0) * (model_config$lambda * (agent_states == 1) %*% model_config$adjacency_matrix_b) + (agent_states == 1) * (model_config$gamma);
+  if(model_config$network_type == "full"){
+    it <- sum(agent_states == 1);
+    alpha_plusone <- (agent_states == 0) *  (model_config$lambda * it / N) + (agent_states == 1) * (model_config$gamma);
+  }else{
+    alpha_plusone <- (agent_state == 1) * (model_config$gamma) + (agent_state == 0) * (model_config$lambda * (agent_state %*% model_config$adjacency_matrix_b));
+  }
   return(as.vector(alpha_plusone));
 }
